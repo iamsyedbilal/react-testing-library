f2ae6195b4a7d5fd8e374b4a1003cd71361a4e30
iamsyedbilal <syedbilal.dev27@gmail.com>
2025-10-05 14:48:22 +0500

    Testing about the components its input forms

--- src/app.test.js
+++ src/app.test.js
@@ -5,6 +5,22 @@ import App from "./App";
 
 test("render react component", () => {
   render(<App />);
-  const element = screen.getByText(/Testing/);
+  const element = screen.getByText(/Testing/i); //For case sensitive we add i
+  const title = screen.getByTitle(/AI generated image/);
   expect(element).toBeInTheDocument();
+  expect(title).toBeInTheDocument();
+});
+
+// Test case for input box
+test("Input box", () => {
+  render(<App />);
+  let checkInput = screen.getByRole("textbox");
+  let checkInputPlaceholder = screen.getByPlaceholderText(
+    "Enter the image name"
+  );
+  expect(checkInput).toBeInTheDocument();
+  expect(checkInputPlaceholder).toBeInTheDocument();
+  expect(checkInput).toHaveAttribute("name", "Image name");
+  //`value` prop to a form field without an `onChange` handler. This will render a read-only field. If the field should be mutable use `defaultValue`.
+  expect(checkInput).toHaveAttribute("value", "anime");
 });
